c9554c1ba36f7fbe1c156b1ac8947819e6dbe711
Margot <mopigames2.0@gmail.com>
2023-06-07 18:11:25 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "  ██████ ██░ ██ ▄▄▄      ██▀███ ▓█████  ▄████ ██▀███  ██▓███▄ ▄███▓
+▒██    ▒▓██░ ██▒████▄   ▓██ ▒ ██▓█   ▀ ██▒ ▀█▓██ ▒ ██▓██▓██▒▀█▀ ██▒
+░ ▓██▄  ▒██▀▀██▒██  ▀█▄ ▓██ ░▄█ ▒███  ▒██░▄▄▄▓██ ░▄█ ▒██▓██    ▓██░
+  ▒   ██░▓█ ░██░██▄▄▄▄██▒██▀▀█▄ ▒▓█  ▄░▓█  ██▒██▀▀█▄ ░██▒██    ▒██ 
+▒██████▒░▓█▒░██▓▓█   ▓██░██▓ ▒██░▒████░▒▓███▀░██▓ ▒██░██▒██▒   ░██▒
+▒ ▒▓▒ ▒ ░▒ ░░▒░▒▒▒   ▓▒█░ ▒▓ ░▒▓░░ ▒░ ░░▒   ▒░ ▒▓ ░▒▓░▓ ░ ▒░   ░  ░
+░ ░▒  ░ ░▒ ░▒░ ░ ▒   ▒▒ ░ ░▒ ░ ▒░░ ░  ░ ░   ░  ░▒ ░ ▒░▒ ░  ░      ░
+░  ░  ░  ░  ░░ ░ ░   ▒    ░░   ░   ░  ░ ░   ░  ░░   ░ ▒ ░      ░   
+      ░  ░  ░  ░     ░  ░  ░       ░  ░     ░   ░     ░        ░   
+                                                                   
+
+"
+echo "This script works only on arch linux. NOTE: You need to have the Paru AUR helper installed on your system to install the dependencies."
+paru -S --noconfirm sharenix-git grim slurp mplayer
+mkdir $HOME/.config/sharegrim
+cp ./screenshot.sh $HOME/.config/sharegrim/screenshot.sh
+chmod +x $HOME/.config/sharegrim/screenshot.sh
+echo "Done! The script is located in $HOME/.config/sharegrim/screenshot.sh! Add your config to sharenix, bind the script to a keybind on your compositor config and enjoy :D"
---
+++ screenshot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+imgDate=$(date "+%y%m%d_%H-%M-%S")
+bgColor="ff006133"
+if [ -d "$HOME/screenshots/" ]
+  then
+    isSet=$(grim -g "$(slurp -b $bgColor)" "$HOME/screenshots/screenshot-$imgDate.png" 2>&1)
+  else
+    mkdir $HOME/screenshots/
+    isSet=$(grim -g "$(slurp -b $bgColor)" "$HOME/screenshots/screenshot-$imgDate.png" 2>&1)
+fi
+if [ -z ${isSet} ]
+  then
+    mplayer https://www.myinstants.com/media/sounds/iphone-screenshot.mp3
+    output=$(sharenix -m=f $HOME/screenshots/screenshot-$imgDate.png)
+    scrurl=$(echo $output | grep -o "http[s]\?://[^[:space:]]\+")
+      if [ -z ${scrurl} ];
+        then
+         notify-send "ShareNIX - Failed" "There was an error sending your screenshot to your file hoster. check your internet connection and your config file then try again later."
+        else
+          wl-copy $scrurl
+          mplayer https://www.myinstants.com/media/sounds/sentmessage_1.mp3
+          notify-send -i $HOME/screenshots/screenshot-$imgDate.png "ShareNIX - Completed" "Screenshot pasted in the clipboard! URL: $scrurl"
+      fi
+fi
